ac0e6dde791020a1633401f4c47475e930cfebbb
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-08-03 05:39:59 +0300

    Python - Input/Output task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+"""Student module"""
 
 
 class Student:
-    """Representation of a student"""
+    """Student class
+    Attributes:
+        first_name (str): the student first name
+        last_name (str): the student last name
+        age (int): the student age
+    """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """Initailazer"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
-
-    def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+    def to_json(self):
+        """Returns the json representation of instance"""
+        return self.__dict__
