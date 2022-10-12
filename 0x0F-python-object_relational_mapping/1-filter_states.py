d34346fdc75809c06b86302580b4fa4d00249e69
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-10-12 05:54:41 +0300

    mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""
-SelectStates module
-"""
-import MySQLdb
-import sys
+"""lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 
+if __name__ == '__main__':
 
-def filter_states():
-    """Filters states from database"""
+    import MySQLdb
+    import sys
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database
-                         )
     cur = db.cursor()
-    cur.execute("SELECT *\
-                FROM states\
-                WHERE name LIKE BINARY 'N%'\
-                ORDER BY id ASC"
-                )
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
-
-if __name__ == "__main__":
-    filter_states()
