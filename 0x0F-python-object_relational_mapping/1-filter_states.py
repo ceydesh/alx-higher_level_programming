1b49e58ad5343d5ae386ec40baa627badbf2c7a1
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-10-15 05:04:14 +0300

    mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa
-where name matches the argument"""
+"""lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
@@ -12,8 +11,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY states.id ASC".format(sys.argv[4]))
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
