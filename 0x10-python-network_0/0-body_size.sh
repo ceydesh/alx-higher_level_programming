865c6dbfd43c451ac5de0f8411096a8bee36eba4
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-10-22 05:57:39 +0300

    network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the size of the body of the response in bytes
-curl -Is "$1" | grep 'Content-Length' | cut -d ' ' -f 2
+# size of the body of the response from request to URL
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
