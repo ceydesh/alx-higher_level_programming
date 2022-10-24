21ba252bf0e2dd21edb6851220ab2323f7135a12
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-10-24 04:54:58 +0300

    Net1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""A script tha:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
 """
-import sys
+Takes in a letter and sends a POST request to a URL
+Uses the letter as a parameter
+"""
 import requests
+from sys import argv
 
 
-if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+def json_api():
+        try:
+            q = argv[1]
+        except:
+            q = ''
+        url = 'http://0.0.0.0:5000/search_user'
+        payload = {'q': q}
+        r = requests.post(url, payload)
+        try:
+            json = r.json()
+            if len(json) == 0:
+                print('No result')
+            else:
+                json_id = json.get('id')
+                json_name = json.get('name')
+                print('[{}] {}'.format(json_id, json_name))
+        except:
+            print('Not a valid JSON')
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    try:
-        response = r.json()
-        if response == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-    except ValueError:
-        print("Not a valid JSON")
+
+if __name__ == '__main__':
+    json_api()
