8ae434f58e851d172e029a84b6876c3f6071f5be
Derrick Henson <84587372+ceydesh@users.noreply.github.com>
2022-10-24 04:58:58 +0300

    Net1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""
-Takes in a letter and sends a POST request to a URL
-Uses the letter as a parameter
+"""script that takes in a letter and
+sends a POST request to
+http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
 import requests
-from sys import argv
-
-
-def json_api():
-        try:
-            q = argv[1]
-        except:
-            q = ''
-        url = 'http://0.0.0.0:5000/search_user'
-        payload = {'q': q}
-        r = requests.post(url, payload)
-        try:
-            json = r.json()
-            if len(json) == 0:
-                print('No result')
-            else:
-                json_id = json.get('id')
-                json_name = json.get('name')
-                print('[{}] {}'.format(json_id, json_name))
-        except:
-            print('Not a valid JSON')
-
+import sys
 
 if __name__ == '__main__':
-    json_api()
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) != 2:
+        print("No result")
+    else:
+        letter = sys.argv[1]
+        if letter:
+            data = {'q': sys.argv[1]}
+        else:
+            data = {'q': ''}
+        response = requests.post(url, data)
+        try:
+            json_response = response.json()
+            if not json_response.get('id'):
+                print("No result")
+            else:
+                print(f"[{json_response.get('id')}] {json_response.get('name')}")
+        except:
+            print("Not a valid JSON")
